ca26f566ac04d7fdd38884750498d6c75a2a8c71
i18n-project <i18n-project@example.com>
2025-07-07 14:42:43 +0900

    remove: 불필요한 클래스 삭제제

--- src/main/java/scouter/plugin/server/alert/slack/ObjectHistory.java
+++
@@ -1,8 +0,0 @@
-package scouter.plugin.server.alert.slack;
-
-public class ObjectHistory extends AlertHistory {
-
-	public ObjectHistory(long lastModified, int historyCount) {
-		super(lastModified, historyCount);
-	}
-}
